5eeb579fcb1921f1aacfb9ad36786d7d960339b4
Ban Victor <banvictorspam@gmail.com>
2023-04-11 21:28:42 +0300

    fixed issue with lines

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 async function generateImage(code, lines) {
-  if (lines !== true) lines = false
+  if (lines != true) lines = false
   const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"]/*,  executablePath: './chrome-linux'*/});
   const page = await browser.newPage();
   await page.goto(`https://carbon.now.sh/?bg=rgba%28248%2C231%2C28%2C1%29&t=blackboard&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=${lines}&fl=1&fm=Fira+Code&fs=14px&lh=152%25&si=false&es=2x&wm=false&code=${encodeURIComponent(code)}`);
