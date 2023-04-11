45d0baef815dd1f559ed99c084695ccea66c77fa
Ban Victor <banvictorspam@gmail.com>
2023-04-11 18:14:03 +0300

    Fixed massive bugs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const puppeteer = require("puppeteer");
+
 async function generateImage(code, lines) {
   if (lines !== true) lines = false
   const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"]/*,  executablePath: './chrome-linux'*/});
