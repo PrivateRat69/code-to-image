4a7fce55078e99b5fbde7188b3406aabe09b50a2
Ban Victor <banvictorspam@gmail.com>
2023-04-12 19:08:57 +0300

    Added option to change background

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const puppeteer = require("puppeteer");
 
-async function generateImage(code, lines) {
+function rgbStringToArray(rgbaString) {
+  const rgbArray = rgbaString.replace(/[^\d,.]/g, '').split(',');
+  return rgbArray;
+}
+
+async function generateImage(code, lines, rgba) {
   if (lines.toLowerCase().startsWith("true")) {lines == true} else lines == false;
   const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"]/*,  executablePath: './chrome-linux'*/});
   const page = await browser.newPage();
-  await page.goto(`https://carbon.now.sh/?bg=rgba%28222%2C171%2C99%2C1%29&t=duotone-dark&wt=sharp&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=${lines}&fl=1&fm=Fira+Code&fs=14px&lh=152%25&si=false&es=2x&wm=false&code=${encodeURIComponent(code)}`);
+  await page.goto(`https://carbon.now.sh/?bg=rgba%28${rgbaStringToArray(rgba)[0]}%2C${rgbaStringToArray(rgba)[1]}%2C${rgbaStringToArray(rgba)[2]}%2C1%29&t=duotone-dark&wt=sharp&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=${lines}&fl=1&fm=Fira+Code&fs=14px&lh=152%25&si=false&es=2x&wm=false&code=${encodeURIComponent(code)}`);
   const element = await page.$('.container-bg');
   const buffer = await element.screenshot({ encoding: 'binary' });
   await browser.close();
